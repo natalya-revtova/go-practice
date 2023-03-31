4689273ae539b2d94061a6679f6f49fa7ddda0ba
Natalya Revtova <52577679+natalya-revtova@users.noreply.github.com>
2023-03-31 15:40:49 +0300

    HW1 is completed (#1)

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	initialString := "Hello, OTUS!"
+	reversedString := stringutil.Reverse(initialString)
+	fmt.Println(reversedString)
 }
