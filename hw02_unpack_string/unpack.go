af56817ac9e4e74e4c9b68edfb6020934e903c0c
n.revtova <revtovagn@gmail.com>
2023-04-12 12:48:04 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+
+	"golang.org/x/example/stringutil"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var isNumber bool
+	var repeatCount int
+	var output strings.Builder
+
+	// unpack string from its end to begining
+	for i, char := range stringutil.Reverse(input) {
+		digit, err := strconv.Atoi(string(char))
+		if i == 0 && err != nil {
+			output.WriteString(string(char))
+		}
+
+		if err == nil {
+			if i == len(input)-1 {
+				return "", ErrInvalidString
+			}
+
+			if isNumber {
+				return "", ErrInvalidString
+			}
+
+			repeatCount = digit
+			isNumber = true
+			continue
+		}
+
+		output.WriteString(strings.Repeat(string(char), repeatCount))
+		repeatCount = 1
+		isNumber = false
+	}
+
+	return stringutil.Reverse(output.String()), nil
 }
