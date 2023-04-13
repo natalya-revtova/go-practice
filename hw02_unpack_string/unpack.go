02f11cbff3dc4fdc6bec119798636e116461a3a6
Natalya Revtova <52577679+natalya-revtova@users.noreply.github.com>
2023-04-13 09:29:27 +0300

    HW2 is completed (#2)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var twoDigitsInARow bool
+	var previousChar string
+	var output strings.Builder
+
+	for i, char := range input {
+		currentChar := string(char)
+		repeatCount, err := strconv.Atoi(currentChar)
+		if err != nil {
+			if !twoDigitsInARow {
+				output.WriteString(previousChar)
+			}
+			if i == len(input)-1 {
+				output.WriteString(currentChar)
+			}
+
+			previousChar = currentChar
+			twoDigitsInARow = false
+			continue
+		}
+
+		if i == 0 {
+			return "", ErrInvalidString
+		}
+		if twoDigitsInARow {
+			return "", ErrInvalidString
+		}
+
+		output.WriteString(strings.Repeat(previousChar, repeatCount))
+		twoDigitsInARow = true
+	}
+
+	return output.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,11 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `\\2\\`, expected: `\\\\\`},
+		{input: "\\2\\", expected: "\\\\\\"},
+		{input: "\n5", expected: "\n\n\n\n\n"},
+		{input: " 4 2", expected: "      "},
+		{input: "a0b0", expected: ""},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +34,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "ab00"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
