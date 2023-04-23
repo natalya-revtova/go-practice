e324a014629b39525b8a2925395fc8ba074b14fc
Natalya Revtova <52577679+natalya-revtova@users.noreply.github.com>
2023-04-23 10:44:22 +0300

    HW3 is completed  (#3)

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,38 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	const topWordsCount = 10
+
+	if text == "" {
+		return nil
+	}
+
+	words := strings.Fields(text)
+
+	uniqueWords := make(map[string]int)
+	for _, word := range words {
+		uniqueWords[word]++
+	}
+
+	topWords := make([]string, 0, len(uniqueWords))
+	for word := range uniqueWords {
+		topWords = append(topWords, word)
+	}
+
+	sort.Slice(topWords, func(i, j int) bool {
+		if uniqueWords[topWords[i]] == uniqueWords[topWords[j]] {
+			return topWords[i] < topWords[j]
+		}
+		return uniqueWords[topWords[i]] > uniqueWords[topWords[j]]
+	})
+
+	if len(topWords) < topWordsCount {
+		return topWords
+	}
+	return topWords[:topWordsCount]
 }
