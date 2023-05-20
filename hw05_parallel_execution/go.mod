588f072de498ba45f58f67467ff338299186c1a4
n.revtova <revtovagn@gmail.com>
2023-05-20 18:10:51 +0300

    hw5 is completed

--- hw05_parallel_execution/go.mod
+++ hw05_parallel_execution/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw05_parallel_execution
+module github.com/natalya-revtova/go-practice/hw05_parallel_execution
 
 go 1.19
 
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,77 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var (
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	ErrEmptyTasksList      = errors.New("list of tasks is empty")
+	ErrMinGoroutinesNumber = errors.New("number of goroutines must be posiive")
+	ErrMinErrorsNumber     = errors.New("number of errors can not be negative")
+)
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if len(tasks) == 0 {
+		return ErrEmptyTasksList
+	}
+	if n <= 0 {
+		return ErrMinGoroutinesNumber
+	}
+	if m < 0 {
+		return ErrMinErrorsNumber
+	}
+
+	tasksCh := make(chan Task, len(tasks))
+	resultCh := make(chan error)
+	stopCh := make(chan struct{})
+
+	wg := sync.WaitGroup{}
+	wg.Add(n)
+
+	for _, task := range tasks {
+		tasksCh <- task
+	}
+	close(tasksCh)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			worker(tasksCh, resultCh, stopCh)
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+
+	errorsCounter := 0
+	for err := range resultCh {
+		if err != nil {
+			errorsCounter++
+		}
+		if errorsCounter == m {
+			close(stopCh)
+		}
+	}
+
+	if errorsCounter >= m {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
+}
+
+func worker(tasks <-chan Task, results chan<- error, stop <-chan struct{}) {
+	for task := range tasks {
+		select {
+		case <-stop:
+			return
+		default:
+			results <- task()
+		}
+	}
 }
