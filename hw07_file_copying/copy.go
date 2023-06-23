e9386be3dc797413b11c12dc1e092173a9fcaa89
n.revtova <revtovagn@gmail.com>
2023-06-23 15:01:18 +0300

    hw07 is comleted

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 var (
@@ -10,6 +15,51 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == "" || toPath == "" || fromPath == toPath {
+		return ErrUnsupportedFile
+	}
+
+	sourceFileStat, err := os.Stat(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	sourceFileSize := sourceFileStat.Size()
+	if sourceFileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 || limit > sourceFileSize-offset {
+		limit = sourceFileSize - offset
+	}
+
+	source, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("failed to open source file: %w", err)
+	}
+	defer source.Close()
+
+	destination, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("failed to create destination file: %w", err)
+	}
+	defer destination.Close()
+
+	_, err = source.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("failed to read source file: %w", err)
+	}
+
+	bar := progressbar.DefaultBytes(limit)
+
+	_, err = io.CopyN(io.MultiWriter(destination, bar), source, limit)
+	if err != nil && errors.Is(err, io.EOF) {
+		return fmt.Errorf("failed to copy data from source file to destination file: %w", err)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,82 @@
 package main
 
-import "testing"
+import (
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/require"
+)
+
+type testCase struct {
+	src    string
+	dest   string
+	offset int64
+	limit  int64
+	want   string
+}
+
+func TestCopy_Success(t *testing.T) {
+	tests := []testCase{
+		{"testdata/input.txt", "/tmp/out.txt", 0, 0, "testdata/out_offset0_limit0.txt"},
+		{"testdata/input.txt", "/tmp/out.txt", 0, 10, "testdata/out_offset0_limit10.txt"},
+		{"testdata/input.txt", "/tmp/out.txt", 0, 1000, "testdata/out_offset0_limit1000.txt"},
+		{"testdata/input.txt", "/tmp/out.txt", 0, 10000, "testdata/out_offset0_limit10000.txt"},
+		{"testdata/input.txt", "/tmp/out.txt", 100, 1000, "testdata/out_offset100_limit1000.txt"},
+		{"testdata/input.txt", "/tmp/out.txt", 6000, 1000, "testdata/out_offset6000_limit1000.txt"},
+	}
+
+	for _, tt := range tests {
+		err := Copy(tt.src, tt.dest, tt.offset, tt.limit)
+		require.NoError(t, err)
+
+		hashOutResult, _ := getHash(tt.dest)
+		hashOutTest, _ := getHash(tt.want)
+		require.Equal(t, hashOutResult, hashOutTest)
+
+	}
+}
+
+func TestCopy_InvalidFiles(t *testing.T) {
+	tests := []testCase{
+		{"", "/tmp/out.txt", 0, 0, ""},
+		{"testdata/input.txt", "", 0, 0, ""},
+		{"testdata/input.txt", "testdata/input.txt", 0, 0, ""},
+		{"testdata/", "/tmp/out.txt", 0, 0, ""},
+		{"/dev/urandom", "/tmp/out.txt", 0, 0, ""},
+	}
+
+	for _, tt := range tests {
+		err := Copy(tt.src, tt.dest, tt.offset, tt.limit)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual err - %v", err)
+	}
+}
+
+func TestCopy_InvalidOffset(t *testing.T) {
+	tests := []testCase{
+		{"testdata/input.txt", "/tmp/out.txt", 10000, 0, ""},
+	}
+
+	for _, tt := range tests {
+		err := Copy(tt.src, tt.dest, tt.offset, tt.limit)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+	}
+}
+
+func getHash(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return nil, fmt.Errorf("failed to create hash: %w", err)
+	}
+
+	return hash.Sum(nil), nil
 }
