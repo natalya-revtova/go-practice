e5f3f4ef636f6a37b0ad455301ec2a23460aceaf
n.revtova <revtovagn@gmail.com>
2023-08-09 15:49:35 +0300

    add index

--- hw12_13_14_15_calendar/migrations/20230720183009_create_event.sql
+++ hw12_13_14_15_calendar/migrations/20230720183009_create_event.sql
@@ -13,4 +13,8 @@ CREATE TABLE events
     week                timestamp NOT NULL,
     month               timestamp NOT NULL
 );
+
+CREATE INDEX events_day_index ON events (day);
+CREATE INDEX events_week_index ON events (week);
+CREATE INDEX events_month_index ON events (month);
 -- +goose StatementEnd
